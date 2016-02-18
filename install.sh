dec3cde4fae29912186ad02c92c9f0fdb3b6572a
matsub <matsub.rk@gmail.com>
2016-02-19 06:09:28 +0900

    :add: installer; os ditector

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+function osname(){
+case `uname | lower` in
+    *'linux'*) OSIS='linux';;
+    *'darwin'*) OSIS='darwin';;
+    *'bsd'*) OSIS='bsd';;
+    *) OSIS='unknown';;
+esac
+}
+
 if [[ -e ~/.dotfiles ]]; then
     git clone https://github.com/matsub/.dotfiles.git ~/.dotfiles
     pushd ~/.dotfiles
@@ -14,4 +23,6 @@ git submodule init
 git submodule update
 
 vim -u ~/.vimrc -i NONE -c "try | NeoBundleUpdate! | finally | q! | endtry" -e -s -V1
+
+
 popd
