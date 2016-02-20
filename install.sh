89901141c5fa0852b026b06f22770e6673289954
matsub <matsub.rk@gmail.com>
2016-02-21 08:03:28 +0900

    :fix: install script

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ esac
 
 if [[ -e .dotfiles ]]; then
     git clone https://github.com/matsub/.dotfiles.git
-    pushd ~/.dotfiles
+    pushd .dotfiles
 else
     pushd .dotfiles
     git pull origin master:master
@@ -23,6 +23,5 @@ git submodule init
 git submodule update
 
 vim -u ~/.vimrc -i NONE -c "try | NeoBundleUpdate! | finally | q! | endtry" -e -s -V1
-
 
 popd
