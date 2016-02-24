13fece9428ee63df8a28b9dde50669e409d32557
matsub.rk <matsub.rk@gmail.com>
2016-02-24 20:18:04 +0900

    fix installing homebrew; took wrong with pipefail

--- brew/install.sh
+++ brew/install.sh
@@ -7,6 +7,6 @@ echo "$password" | sudo -S chown `whoami`:staff /opt/homebrew
 curl -L https://github.com/Homebrew/homebrew/tarball/master | tar xz --strip 1 -C /opt/homebrew
 }
 
-type brew &> /dev/null && install_brew
+type brew &> /dev/null || install_brew
 
 brew bundle
