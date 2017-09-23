df29d30911573f0bd59fa0db3f306cc1df2a718a
matsub <matsub.rk@gmail.com>
2017-09-23 13:55:45 +0900

    several config on fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,8 @@
+set -x XDG_CONFIG_HOME $HOME/.config
+set -x PATH $PATH $XDG_CONFIG_HOME/yarn/global/node_modules/.bin
+set -x LANG en_US.UTF-8
+set -x HOMEBREW_CASK_OPTS "--caskroom=/usr/local/Caskroom"
+
 alias l="ls -lG"
 alias la="ls -lGa"
 alias vi="nvim"
