5ec07e595367468f341706c44c916d49fe757b1e
matsub <matsub.rk@gmail.com>
2017-09-23 15:06:14 +0900

    fxxh

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,7 +11,15 @@ alias py="python"
 alias tk="tmux kill-session -t"
 
 alias stime="/usr/bin/time -p"
-alias reload="exec -l $SHELL"
+alias reload=". .config/fish/config.fish"
 alias randgen="openssl rand -base64"
 
 fish_default_key_bindings
+
+anyenv init - fish > tmp
+source tmp
+rm tmp
+eval (docker-machine env docker)
+
+# additional environment managers
+eval (pyenv virtualenv-init - | source)
