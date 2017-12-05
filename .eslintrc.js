f4ae26c5d440c52d23bef1b48aefd9905a50ba9a
matsub <matsub.rk@gmail.com>
2017-12-05 16:24:06 +0900

    add eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  // add your custom rules here
+  'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow dangling comma
+    'comma-dangle': ['error', 'only-multiline'],
+  }
+}
