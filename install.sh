5e95a1adaf777000ae93656ba001df3d8395066a
matsub <matsub.rk@gmail.com>
2018-06-17 00:35:00 +0900

    i ate my hands

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 source lib/pick.sh
+source lib/checkin.sh
 source lib/title.sh
 
+# get the location of dotfiles dir
+repo=`checkin`
+echo ""
+pushd $repo > /dev/null
+
+
+# make a choice
 menu=(
 "1) Install dotfiles only"
 "2) Install dotfiles and its plugin manager"
@@ -11,24 +19,40 @@ menu=(
 )
 
 choice=`pick $menu`
+echo ""
+case $choice in
+    0) # Install dotfiles only
+        make dotfiles
+        ;;
+    1) # Install dotfiles and its plugin manager
+        make environment
+        ;;
+    2) # Install all environments
+        make all
+        ;;
+    3) # Remove this dotfiles
+        make clean
+        popd > /dev/null
+        ;;
+    4) # Cancel
+        popd > /dev/null
+        exit
+        ;;
+esac
 
 
-if [ -z $MDOTDIR ]; then
-    # cloning repo
-    git clone --depth 1 https://github.com/matsub/dotfiles.git
-    pushd dotfiles
-else
-    # update dotfiles
-    pushd $MDOTDIR
-fi
-
-
+# completed!
 # change login shell to zsh
-echo "Install completed!"
-echo "please change login shell to zsh with:"
+echo ""
+printf "\e[1m"
+echo " Install completed!"
+echo "===================="
+printf "\e[m"
+echo "please change login shell with:"
 echo "  chsh -s /bin/zsh"
 echo "or"
 echo "  chsh -s /bin/fish"
+echo ""
 
-# completed
-popd
+# back to initial path
+popd > /dev/null
