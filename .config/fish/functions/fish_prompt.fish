90ab65483a627a4238d002f38c76047ddd4d214f
matsub <matsub.rk@gmail.com>
2018-07-17 05:42:13 +0900

    add rprompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -9,18 +9,30 @@ function fish_prompt
     and echo (set_color red)"#"
 
     # define colors
-    set -g orange (set_color EFA400)
     set -g base (set_color EFA400)
     set -g ok (set_color 80BB70)
     set -g ng (set_color FF3333)
 
     # Main
-    # echo -n (set_color white)(basename (tty))
     if [ $last_status -ne 0 ]
         echo -n $ng";_; "
     else
         echo -n $ok"._. "
     end
     echo -n $base(basename $PWD)" "
-    echo -n $orange(__fish_vcs_prompt) (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+    echo -n (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+end
+
+function fish_right_prompt
+    # define colors
+    set -g orange (set_color EFA400)
+    set -g gray (set_color 303030)
+    set -g red (set_color CF4F4F)
+    set -g reset_color (set_color normal)
+
+    # Main
+    echo -n $orange(__fish_vcs_prompt)
+    echo -n $gray' '(date "+%b %H %T")
+    #echo -n $red' @'$gray(hostname)
+    echo -n $reset_color
 end
