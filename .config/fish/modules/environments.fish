737dd726dc23578fd595bc5fa990846134082561
matsub <matsub.rk@gmail.com>
2019-10-03 14:06:24 +0900

    syake

--- .config/fish/modules/environments.fish
+++ .config/fish/modules/environments.fish
@@ -7,6 +7,11 @@ set -x PATH $PATH $GOPATH/bin
 set -x PATH $PATH /usr/local/src/depot_tools
 set -x PATH $PATH /usr/local/Cellar/terraform/0.12.2/bin/
 
+set -x QTDIR (brew --prefix qt5)
+set -x C_INCLUDE_PATH "/usr/local/include/glib-2.0"
+set -x CPLUS_INCLUDE_PATH "/usr/local/include/glib-2.0"
+set -x LIBRARY_PATH "/usr/local/include/glib-2.0"
+
 set -x LANG en_US.UTF-8
 set -x PROJECT_ID (gcloud config get-value project -q)
 
